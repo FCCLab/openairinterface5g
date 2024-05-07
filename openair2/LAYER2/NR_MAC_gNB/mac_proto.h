c2fc49cf4ce9ec46d682725bcedb1d62e4d820d9
Thomas Schlichter <thomas.schlichter@iis.fraunhofer.de>
2024-05-07 18:00:53 +0200

    gNB: add NTN specific parameter cellSpecificKoffset_r17 and use it for UL scheduling
    According to the RRC specification, cellSpecificKoffset is:
    Scheduling offset used for the timing relationships that are modified for NTN (see TS 38.213 [13]).
    The unit of the field K_offset is number of slots for a given subcarrier spacing of 15 kHz.
    If the field is absent UE assumes value 0
    
    This parameter `cellSpecificKoffset_r17` can be set in the gNB conf file, in the section `servingCellConfigCommon`

--- openair2/LAYER2/NR_MAC_gNB/mac_proto.h
+++ openair2/LAYER2/NR_MAC_gNB/mac_proto.h
@@ -37,6 +37,7 @@
 
 void set_cset_offset(uint16_t);
 void get_K1_K2(int N1, int N2, int *K1, int *K2, int layers);
+int get_NTN_Koffset(const NR_ServingCellConfigCommon_t *scc);
 
 void mac_top_init_gNB(ngran_node_t node_type,
                       NR_ServingCellConfigCommon_t *scc,
@@ -264,7 +265,8 @@ NR_SearchSpace_t *get_searchspace(NR_ServingCellConfigCommon_t *scc,
 
 long get_K2(NR_PUSCH_TimeDomainResourceAllocationList_t *tdaList,
             int time_domain_assignment,
-            int mu);
+            int mu,
+            const NR_ServingCellConfigCommon_t *scc);
 
 const NR_DMRS_UplinkConfig_t *get_DMRS_UplinkConfig(const NR_PUSCH_Config_t *pusch_Config, const NR_tda_info_t *tda_info);
 
