5567025bd95774c0bef985d32cfafdd768e9a884
Van Tuan Ngo <vantuan_ngo@sutd.edu.sg>
2024-07-02 15:20:33 +0000

    FCP

--- ci-scripts/yaml_files/sa_gnb_aerial/aerial_l1_entrypoint.sh
+++ ci-scripts/yaml_files/sa_gnb_aerial/aerial_l1_entrypoint.sh
@@ -35,4 +35,8 @@ else
 fi
 
 sudo -E "$cuBB_Path"/build/cuPHY-CP/cuphycontroller/examples/cuphycontroller_scf "$argument"
-sudo ./build/cuPHY-CP/gt_common_libs/nvIPC/tests/pcap/pcap_collect
+sudo -E ./build/cuPHY-CP/gt_common_libs/nvIPC/tests/pcap/pcap_collect
+
+# while true; do
+#     sleep 1
+# done
