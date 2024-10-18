cfa7c06eb00a899c431847dbadd93e2d94403e16
Van Tuan Ngo <vantuan_ngo@sutd.edu.sg>
2024-10-18 22:26:59 +0800

    Use clang-format

--- openair2/F1AP/f1ap_cu_ue_context_management.c
+++ openair2/F1AP/f1ap_cu_ue_context_management.c
@@ -108,8 +108,7 @@ static void f1ap_write_drb_nssai(const nssai_t *nssai, F1AP_SNSSAI_t *asn1_nssai
   OCTET_STRING_fromBuf(&asn1_nssai->sST, (char *)&nssai->sst, 1);
 
   /* OPTIONAL */
-  if (nssai->sd != 0xffffff)
-  {
+  if (nssai->sd != 0xffffff) {
     char sd_buffer[3];
     INT24_TO_BUFFER(nssai->sd, sd_buffer);
     asn1cCalloc(asn1_nssai->sD, sD);
