8ecd14884e5bf48c53f09da782cd080666d789fc
Le Thanh Long <thanhlongvt98@gmail.com>
2025-01-08 17:36:47 +0800

    Add comment (effort to try maximize the mcs)

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
@@ -757,7 +757,8 @@ int get_mcs_from_bler(const NR_bler_options_t *bler_options,
   bler_stats->bler = BLER_FILTER * bler_stats->bler + (1 - BLER_FILTER) * bler_window;
 
   int new_mcs = old_mcs;
-  if (bler_stats->bler < bler_options->lower && old_mcs < max_mcs && num_dl_sched > 3)
+  // if (bler_stats->bler < bler_options->lower && old_mcs < max_mcs && num_dl_sched > 3)
+  if (bler_stats->bler < bler_options->lower && old_mcs < max_mcs)
     new_mcs += 1;
   else if ((bler_stats->bler > bler_options->upper && old_mcs > 6) // above threshold
       || (num_dl_sched <= 3 && old_mcs > 9))                                // no activity
