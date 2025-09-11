d44a6455ecd53ec3ed786075b7585741c80530bc
Tuan V. Ngo <tuannv.email@gmail.com>
2025-09-12 00:16:11 +0800

    X410 work with Pegatron Dongle

---
+++ x410/gNB_106PRB_2x2_X410.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo ifconfig enp12s0f0np0 192.168.10.100/24
+sudo ifconfig enp12s0f0np mtu 9000
+
+sudo cpupower idle-set -D 0
+
+for ((i=0;i<$(nproc);i++)); do sudo cpufreq-set -c $i -r -g performance; done
+sudo sysctl -w net.core.wmem_max=62500000
+sudo sysctl -w net.core.rmem_max=62500000
+sudo sysctl -w net.core.wmem_default=62500000
+sudo sysctl -w net.core.rmem_default=62500000
+sudo ethtool -G enp12s0f0np tx 4096 rx 4096
+
+sudo ../cmake_targets/ran_build/build/nr-softmodem -O ./gnb.sa.band78.fr1.106PRB.2x2.usrpx410.352501.conf
